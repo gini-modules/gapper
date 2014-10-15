fa707f31ebd3672077810a227a8cd898e945d5b5
Jia Huang <jia.huang@geneegroup.com>
2014-10-15 05:57:12 +0000

    use logger in RPCTrait

--- class/Gini/Module/Gapper/Client/RPCTrait.php
+++ class/Gini/Module/Gapper/Client/RPCTrait.php
@@ -16,10 +16,11 @@ namespace Gini\Module\Gapper\Client
                     $rpc = \Gini\IoC::construct('\Gini\RPC', $api, $type);
                     $bool = $rpc->authorize($client_id, $client_secret);
                     if (!$bool) {
-                        throw new \Exception('Your APP was not registered in gapper server!');
+                        throw new \Exception('Your app was not registered in gapper server!');
                     }
                     self::$_RPCs[$type] = $rpc;
                 } catch (\Gini\RPC\Exception $e) {
+                    \Gini\Logger::of('gapper')->error('Gapper::getRPC error: {message}', ['message' => $e->getMessage()]);
                 }
             }
             return self::$_RPCs[$type];
