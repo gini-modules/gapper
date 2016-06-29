73b69437829d4d7efa4dde7af9e319b2b89db289
pihizi <pihizi@msn.com>
2016-06-29 06:02:03 +0000

    try error

--- raw/assets/js/gapper/client/login.js
+++ raw/assets/js/gapper/client/login.js
@@ -5,6 +5,7 @@ define('gapper/client/login', ['jquery', 'bootbox', 'css!../../../css/gapper-cho
         if (!dialog || ! dialog.length) return;
         dialog.prev('.modal-backdrop').remove();
         dialog.remove();
+        dialog = undefined;
     };
     var clearLoadingDialog = function() {
         if (!loadingDialog || ! loadingDialog.length) return;
@@ -21,10 +22,10 @@ define('gapper/client/login', ['jquery', 'bootbox', 'css!../../../css/gapper-cho
     };
     var showDialog = function(html, callback) {
         clearDialog();
-        dialog = $('<div></div>');
-        dialog.html(html);
-        dialog = dialog.find('.modal');
-        if (!dialog.length) {
+        try {
+            dialog = $(html);
+        } catch(err) {}
+        if (!dialog || ! dialog.hasClass('modal')) {
             dialog = $('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><button class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button><h4 class="modal-title"></h4></div></div></div></div>');
             dialog.html(html);
         }
