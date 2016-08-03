248ad0915e3b7bad95cc907a78950d83631272b2
pihizi <pihizi@msn.com>
2016-08-03 11:13:41 +0000

    代码还原，不知道出了什么问题

--- raw/assets/js/gapper/client/utils/require.js
+++ raw/assets/js/gapper/client/utils/require.js
@@ -1,36 +1,30 @@
 define('gapper/client/utils/require', ['jquery'], function($) {
-    var run = function() {
-        $('[data-require]').each(function() {
-            var attr = $(this).attr('data-require');
-            attr = attr.split(',');
-            require(attr, function() {
-                var args = Array.prototype.slice.call(arguments);
-                for (var i = 0, l = args.length; i < l; i++) {
-                    if (args[i] && args[i].loopMe) {
-                        args[i].loopMe();
-                    }
-                }
-            });
-        });
-    };
-
-    function init() {
-        if (document.implementation.hasFeature('MutationsEvents', '2.0')) {
-            $('body').on('DOMNodeInserted', function(evt) {
-                run();
-            });
-        }
-        else {
-            setInterval(run, 1000);
-        }
-    }
-
-    var clock = setTimeout(function() {
-        init();
-    }, 2000);
-
 	$(function() {
-        clearTimeout(clock);
-        init();
+		var run = function() {
+			$('[data-require]').each(function() {
+				var attr = $(this).attr('data-require');
+				attr = attr.split(',');
+				require(attr, function() {
+					var args = Array.prototype.slice.call(arguments);
+					for (var i = 0, l = args.length; i < l; i++) {
+						if (args[i] && args[i].loopMe) {
+							args[i].loopMe();
+						}
+					}
+				});
+			});
+		};
+
+		if (document.implementation.hasFeature('MutationsEvents', '2.0')) {
+			$('body').on('DOMNodeInserted', function(evt) {
+				run();
+			});
+		}
+		else {
+			setInterval(run, 1000);
+		}
+
+		run();
 	});
 });
+
