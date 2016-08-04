4273d47b0d68a2aa03a0782f165b26ad63db2c9f
pihizi <pihizi@msn.com>
2016-08-04 01:52:53 +0000

    uglyjs应该有bug？不知道为什么代码始终会压缩出异常字符

--- raw/assets/js/gapper/client/utils/require.js
+++ raw/assets/js/gapper/client/utils/require.js
@@ -1,30 +1,39 @@
 define('gapper/client/utils/require', ['jquery'], function($) {
-	$(function() {
-		var run = function() {
-			$('[data-require]').each(function() {
-				var attr = $(this).attr('data-require');
-				attr = attr.split(',');
-				require(attr, function() {
-					var args = Array.prototype.slice.call(arguments);
-					for (var i = 0, l = args.length; i < l; i++) {
-						if (args[i] && args[i].loopMe) {
-							args[i].loopMe();
-						}
-					}
-				});
-			});
-		};
+    var run = function() {
+        $('[data-require]').each(function() {
+            var attr = $(this).attr('data-require');
+            attr = attr.split(',');
+            require(attr, function() {
+                var args = Array.prototype.slice.call(arguments);
+                for (var i = 0, l = args.length; i < l; i++) {
+                    if (args[i] && args[i].loopMe) {
+                        args[i].loopMe();
+                    }
+                }
+            });
+        });
+    };
 
-		if (document.implementation.hasFeature('MutationsEvents', '2.0')) {
-			$('body').on('DOMNodeInserted', function(evt) {
-				run();
-			});
-		}
-		else {
-			setInterval(run, 1000);
-		}
+    function init() {
+        if (document.implementation.hasFeature('MutationsEvents', '2.0')) {
+            $('body').on('DOMNodeInserted', function(evt) {
+                run();
+            });
+        }
+        else {
+            setInterval(run, 1000);
+        }
 
-		run();
-	});
+        run();
+    }
+
+    var clock = setTimeout(function() {
+        init();
+    }, 2000);
+
+    $(function() {
+        clearTimeout(clock);
+        init();
+    });
 });
 
