a268b2329927ced9c1f301598aab1b121287819b
洪小兵 <pihizi@msn.com>
2019-01-29 06:13:38 +0000

    add: 如果本地有代理的server的app信息，就直接在本地拿，不要再远程rpc获取了。labmai这边涉及一个域名转path的问题，在多域名转多运营的时候，需要考虑本地代理数据的更新

--- class/Gini/ORM/Gapper/Agent/User.php
+++ class/Gini/ORM/Gapper/Agent/User.php
@@ -9,7 +9,7 @@ class User extends \Gini\ORM\Gapper\Agent\SObject
     public $username = 'string:120,comment:用户名';
     public $email = 'string:120,comment:email';
     public $phone = 'string:120,comment:电话';
-    public $icon = 'string:*,comment:用户头像';
+    public $icon = 'string:250,comment:用户头像';
     public $stime = 'datetime,comment:同步时间';
 
     protected static $db_index = [
